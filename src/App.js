495b81e263899d36667fb3d01982d0f980285b03
Kha Truong <khatruong2009@gmail.com>
2020-10-07 18:30:58 -0400

    more testing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React from 'react';
-
-import Header from './components/Header';
-import Intro from './components/Intro';
-import About from './components/About';
-import Schedule from './components/Schedule';
-import Speakers from './components/Speakers';
-import Subscribe from './components/Subscribe';
-import Gallery from './components/Gallery';
-import Sponsors from './components/Sponsors';
-import FAQ from './components/FAQ';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Intro
-        title='12th North American Leadership Forum'
-        slogan='Bold Vision, Strategic Action'
-        date='October 28, 2020'
-      />
-      <main id='main'>
-        <About />
-        <Schedule />
-        <Speakers />
-        <Subscribe />
-        <Gallery />
-        <Sponsors />
-        <FAQ />
-        <Contact />
-      </main>
-      <Footer />
-      <a href='#' class='back-to-top'>
-        <i class='fa fa-angle-up'></i>
-      </a>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
