38946ecd0d5ea8855aa7e8f461b4f8aafad1efa4
Kha Truong <khatruong2009@gmail.com>
2020-10-07 18:57:38 -0400

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
+import Intro from './components/Intro';
+import About from './components/About';
+import Schedule from './components/Schedule';
+import Speakers from './components/Speakers';
+import Subscribe from './components/Subscribe';
+import Gallery from './components/Gallery';
+import Sponsors from './components/Sponsors';
+import FAQ from './components/FAQ';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro
+        title='12th North American Leadership Forum'
+        slogan='Bold Vision, Strategic Action'
+        date='October 28, 2020'
+      />
     </div>
   );
 }
