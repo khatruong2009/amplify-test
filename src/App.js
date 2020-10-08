4c63f140123498292f1712d6ef7ff04e595dd010
Kha Truong <khatruong2009@gmail.com>
2020-10-07 20:26:46 -0400

    added about component

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ function App() {
         slogan='Bold Vision, Strategic Action'
         date='October 28, 2020'
       />
+      <main id="main">
+        <About />
+      </main>
     </div>
   );
 }
